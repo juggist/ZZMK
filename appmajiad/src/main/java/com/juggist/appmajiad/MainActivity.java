233ac78042c304a436812dfb82fbe1443b6406d9
lufeisong <735596768@qq.com>
2018-12-25 14:54:41 +0800

    1.增加maven 2.增加签名 3.增加多渠道打包

--- appmajiad/src/main/java/com/juggist/appmajiad/MainActivity.java
+++ appmajiad/src/main/java/com/juggist/appmajiad/MainActivity.java
@@ -2,8 +2,11 @@ package com.juggist.appmajiad;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.luojilab.component.componentlib.router.ui.UIRouter;
+import com.meituan.android.walle.WalleChannelReader;
+import com.orhanobut.logger.Logger;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView tv = findViewById(R.id.tv);
+        Logger.d("channel:%s",WalleChannelReader.getChannel(this.getApplicationContext()));
         findViewById(R.id.tv).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
